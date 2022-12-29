27420d721691c3c75e529754e13c106b4f7690b5
phuan516 <31741081+phuan516@users.noreply.github.com>
2022-12-29 12:01:15 -0700

    Add files via upload

---
+++ Player/Player.gd
@@ -0,0 +1,145 @@
+extends KinematicBody2D
+class_name Player
+
+enum {MOVE, CLIMB}
+
+export(Resource) var moveData 
+
+var velocity = Vector2.ZERO
+var state = MOVE
+var double_jump = 1
+var buffer_jump = false
+var coyote_jump = false
+
+onready var animatedSprite: = $AnimatedSprite
+onready var ladderCheck: = $LadderCheck
+onready var jumpBufferTimer: = $jumpBufferTimer
+onready var coyoteJumpTimer: = $coyoteJumpTimer
+onready var remoteTranform2D:= $RemoteTransform2D
+
+func _ready():
+	moveData = moveData as PlayerMovementData
+	
+func _physics_process(delta):	
+	var input = Vector2.ZERO
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	
+	match state:
+		MOVE: move_state(input)
+		CLIMB: climb_state(input)
+	
+func move_state(input):
+	if is_on_ladder() and Input.is_action_pressed("ui_up"):
+		state = CLIMB
+	
+	apply_gravity()
+	if input.x == 0:
+		apply_friction()
+		animatedSprite.animation = 'Idle'
+	else:
+		apply_acceleration(input.x)
+		animatedSprite.animation = 'Run'
+		animatedSprite.flip_h = input.x > 0
+		
+	if is_on_floor():
+		reset_double_jump();
+	else:
+		animatedSprite.animation = 'Jump'
+		
+	if can_jump():
+		input_jump()
+	else:
+		input_jump_release()
+		input_double_jump()
+		buffer_jump()
+		fast_fall()
+	
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	var just_landed = is_on_floor() and was_in_air
+	if just_landed:
+		animatedSprite.animation = 'Run'
+		animatedSprite.frame = 1
+	
+	var just_left_ground = not is_on_floor() and was_on_floor
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyoteJumpTimer.start()
+
+func climb_state(input):
+	if not is_on_ladder():
+		state = MOVE
+	if input.length() != 0:
+		animatedSprite.animation = "Run"
+	else: 
+		animatedSprite.animation = "Idle"
+		
+	velocity = input * moveData.CLIMB_SPEED
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func player_die():
+	SoundPlayer.play_sound(SoundPlayer.HURT)
+	queue_free()
+	Events.emit_signal("player_died")
+
+func connect_camera(camera):
+	var camera_path = camera.get_path()
+	remoteTranform2D.remote_path = camera_path
+
+func input_jump_release():
+	if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+		velocity.y = moveData.JUMP_RELEASE_FORCE
+
+func input_double_jump():
+	if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+		SoundPlayer.play_sound(SoundPlayer.JUMP)
+		velocity.y = moveData.JUMP_FORCE
+		double_jump -= 1
+
+func buffer_jump():
+	if Input.is_action_just_pressed("ui_up"):
+		buffer_jump = true
+		jumpBufferTimer.start()
+	
+func fast_fall():
+	if velocity.y > 0 :
+		velocity.y += moveData.ADDITION_FALL_GRAVITY
+	
+func can_jump():
+	return is_on_floor() or coyote_jump
+	
+func reset_double_jump():
+	double_jump = moveData.DOUBLE_JUMP_COUNT
+
+func input_jump():
+	if Input.is_action_just_pressed("ui_up") or buffer_jump:
+		SoundPlayer.play_sound(SoundPlayer.JUMP)
+		velocity.y = moveData.JUMP_FORCE
+		buffer_jump = false
+	
+func is_on_ladder():
+	if not ladderCheck.is_colliding(): return false
+	var collider = ladderCheck.get_collider()
+	if not collider is Ladder: return false
+	return true
+	
+func apply_gravity():
+	velocity.y += moveData.GRAVITY
+	velocity.y = min(velocity.y, 300)
+
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
+	
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
+
+func _on_jumpBufferTimer_timeout():
+	buffer_jump = false
+	
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
+	
---
+++ README.md
@@ -0,0 +1,27 @@
+# Third Time's a Charm
+
+Think you can complete the level fast enough to end the game?
+
+## Audience & Genre
+
+- Platform Game.
+- Speed Runners.
+- casual Gamers.
+
+## Synopsis
+
+### Theme
+
+- Giant floating testing island where you are the test subject of scientists trying to build the ultimate alien.
+
+### Goals
+
+- Your goal is to complete the level as fast as possible.
+
+### Mechanics
+
+- Each time the fails to complete the level in a fast enough time they are give a boots to their player stat and have to complete the level again until they complete the level in a satisfactory time.
+
+### Uniqueness
+
+- Instead of completing a level once and forgetting about it, This game allows the player to explore the level and find alternative ways to finishing a level.
